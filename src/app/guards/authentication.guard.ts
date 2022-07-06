a8acd05f95a1046ec7bf8bbd64938a1409385ca8
Muhammad Khuzain <mkhuzain17@gmail.com>
2022-07-07 00:33:10 +0800

    update error update status

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ServiceService } from './../services/service.service';
-import { NavController, ModalController, LoadingController, ToastController,Platform } from '@ionic/angular';
+import { NavController, ModalController, LoadingController, ToastController, Platform } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  DataLogin:any;
+  DataLogin: any;
   [x: string]: any;
   constructor(
-    private serviceService:ServiceService,
-      public toastController: ToastController,
-  ){ }
+    private serviceService: ServiceService,
+    public toastController: ToastController,
+    private alertControl: AlertController,
+    private router: Router,
+  ) { }
 
   canActivate() {
-    if(this.serviceService.DataLogin){
+    if (this.serviceService.DataLogin) {
       return true;
     }
-    alert("Maaf, Anda Tidak Punya Akses!!")
+    alert("Maaf, Anda Tidak Punya Akses!!");
+    // this.presentAlertConfirm(); 
     return false;
   }
 
-  
+  async presentAlertConfirm() {
+    const alert = await this.alertControl.create({
+      cssClass: 'my-custom-class',
+      header: 'Perhatian!',
+      message: 'Anda Tidak Punya Akses. Silakan Login',
+      buttons: [
+        {
+          text: 'Okay',
+          handler: () => {
+            this.router.navigate(['login']);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
