4b93dff5be8f25a79f1131f0bdef9775310af4a1
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-11 22:23:32 +0800

    add refresher in dash-housekeeping

--- src/app/dashboard/dash-aduanhk/dash-aduanhk.page.ts
+++ src/app/dashboard/dash-aduanhk/dash-aduanhk.page.ts
@@ -1,16 +1,21 @@
-import { ServiceService } from 'src/app/services/service.service';
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { AlertController, ModalController, LoadingController, ToastController } from '@ionic/angular';
+import { ServiceService } from "src/app/services/service.service";
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import {
+  AlertController,
+  ModalController,
+  LoadingController,
+  ToastController,
+} from "@ionic/angular";
 import { ReplaySubject } from "rxjs";
-import { catchError } from 'rxjs/operators';
-import { UtilService } from 'src/app/services/util.service';
+import { catchError } from "rxjs/operators";
+import { UtilService } from "src/app/services/util.service";
 
 @Component({
-  selector: 'app-dash-aduanhk',
-  templateUrl: './dash-aduanhk.page.html',
-  styleUrls: ['./dash-aduanhk.page.scss'],
+  selector: "app-dash-aduanhk",
+  templateUrl: "./dash-aduanhk.page.html",
+  styleUrls: ["./dash-aduanhk.page.scss"],
 })
 export class DashAduanhkPage implements OnInit {
   [x: string]: any;
@@ -28,8 +33,8 @@ export class DashAduanhkPage implements OnInit {
     public toastController: ToastController,
     private router: Router,
     public util: UtilService,
-    private alertController: AlertController,
-  ) { }
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
     this.getUser();
@@ -41,11 +46,11 @@ export class DashAduanhkPage implements OnInit {
   }
 
   getRecordHousekeeping() {
-    this.serviceService.getRecord('housekeeping/all').subscribe(
-      data => {
+    this.serviceService.getRecord("housekeeping/all").subscribe(
+      (data) => {
         this.DataRecord = data.body;
       },
-      error => {
+      (error) => {
         console.log("err", error);
       }
     );
@@ -53,26 +58,27 @@ export class DashAduanhkPage implements OnInit {
 
   async updateaduan(id: string, status: string, statusInit: string) {
     const loading = await this.loadingController.create({
-      message: 'Please wait...'
+      message: "Please wait...",
     });
     await loading.present();
 
-    const payload = { 'status': status }
+    const payload = { status: status };
 
     if (status === statusInit) {
-      this.presentToast("Edit Status Aduan Catering Sukses")
-    }
-    else {
-      this.serviceService.updateStatus(payload, 'housekeeping/update/', id).subscribe(
-        data => {
-          this.presentToast("Edit Status Aduan Catering Sukses")
-          this.ngOnInit();
-        },
-        error => {
-          this.presentToast("Edit Status Aduan Catering Gagal");
-          console.log(error.message)
-        }
-      );
+      this.presentToast("Edit Status Aduan Catering Sukses");
+    } else {
+      this.serviceService
+        .updateStatus(payload, "housekeeping/update/", id)
+        .subscribe(
+          (data) => {
+            this.presentToast("Edit Status Aduan Catering Sukses");
+            this.ngOnInit();
+          },
+          (error) => {
+            this.presentToast("Edit Status Aduan Catering Gagal");
+            console.log(error.message);
+          }
+        );
     }
     loading.dismiss();
   }
@@ -81,58 +87,58 @@ export class DashAduanhkPage implements OnInit {
     const toast = await this.toastController.create({
       message: Message,
       duration: 2500,
-      position: "top"
+      position: "top",
     });
     toast.present();
   }
 
   onBack() {
-    this.router.navigate(['dashboard']);
+    this.router.navigate(["dashboard"]);
   }
 
   ngOnDestroy() {
-    if (typeof this.routerEvents !== 'undefined') this.routerEvents.unsubscribe();
+    if (typeof this.routerEvents !== "undefined")
+      this.routerEvents.unsubscribe();
   }
 
   async openModal(data) {
     let status: string = data.status;
-    if (this.serviceService.isHasAccess('HOUSEKEEPING', 'COMPLAINT', 'EDIT')) {
+    if (this.serviceService.isHasAccess("HOUSEKEEPING", "COMPLAINT", "EDIT")) {
       const alert = await this.alertController.create({
-        cssClass: 'my-custom-class',
-        header: 'Ubah Status!',
-        message: 'Status sekarang: ' + data.status.slice(0, 8),
+        cssClass: "my-custom-class",
+        header: "Ubah Status!",
+        message: "Status sekarang: " + data.status.slice(0, 8),
         inputs: [
           {
-            name: 'CLEANING',
-            type: 'radio',
-            label: 'CLEANING',
-            value: 'CLEANING_PROGRESS',
+            name: "CLEANING",
+            type: "radio",
+            label: "CLEANING",
+            value: "CLEANING_PROGRESS",
             handler: () => {
-              status = 'CLEANING_PROGRESS'
+              status = "CLEANING_PROGRESS";
             },
-            checked: data.status == 'CLEANING_PROGRESS',
+            checked: data.status == "CLEANING_PROGRESS",
           },
           {
-            name: 'DONE',
-            type: 'radio',
-            label: 'DONE',
-            value: 'DONE',
+            name: "DONE",
+            type: "radio",
+            label: "DONE",
+            value: "DONE",
             handler: () => {
-              status = 'DONE'
+              status = "DONE";
             },
-            checked: data.status == 'DONE',
+            checked: data.status == "DONE",
           },
         ],
         buttons: [
           {
-            text: 'Cancel',
-            role: 'cancel',
-            cssClass: 'secondary',
-            handler: () => {
-            },
+            text: "Cancel",
+            role: "cancel",
+            cssClass: "secondary",
+            handler: () => {},
           },
           {
-            text: 'Ok',
+            text: "Ok",
             handler: () => {
               this.updateaduan(data.id, status, data.status);
             },
@@ -142,5 +148,11 @@ export class DashAduanhkPage implements OnInit {
       await alert.present();
     }
   }
-}
 
+  handleRefresh(event) {
+    setTimeout(() => {
+      this.ngOnInit();
+      event.target.complete();
+    }, 2000);
+  }
+}
