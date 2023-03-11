de52ba68aaa062f13bf52fb4ad92a48a4ed23187
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-03-11 22:01:21 +0800

    add refresher and fix structure aduan laundry

--- src/app/infolaundry/infolaundry.page.ts
+++ src/app/infolaundry/infolaundry.page.ts
@@ -125,4 +125,11 @@ export class InfolaundryPage implements OnInit {
     });
     await alert.present();
   }
+
+  handleRefresh(event) {
+    setTimeout(() => {
+      this.ngOnInit();
+      event.target.complete();
+    }, 2000);
+  }
 }
